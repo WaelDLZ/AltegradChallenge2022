ab1e88744e1fd9d888f5101627ce226efe11159e
Wael <waeldoulazmi@gmail.com>
2023-01-16 19:22:26 +0100

    Bert is fine tuned

--- BERT_inference.py
+++ BERT_inference.py
@@ -13,7 +13,10 @@ import os
 import pickle
 
 tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert_bfd", do_lower_case=False)
-model = BertModel.from_pretrained("Rostlab/prot_bert_bfd")
+model = BertModel.from_pretrained('Prot_bert/')
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
 
 # Load sequences
 
@@ -40,30 +43,36 @@ with torch.no_grad():
     # Get Embeddings per amino acid and per protein
     for seq in tqdm(sequences_train):
         encoded_input = tokenizer(seq, return_tensors='pt')
+        for key in encoded_input.keys():
+            encoded_input[key] = encoded_input[key].to(device)
         output = model(**encoded_input)
-        prot_embedding = output.pooler_output.detach().numpy()[0]
-        amino_embeddings = output.last_hidden_state.detach().numpy()[0]
-        
+        prot_embedding = output.pooler_output.cpu().numpy()[0]
+        amino_embeddings = output.last_hidden_state.cpu().numpy()[0]
+
         train_bert_embeddings["Protein"].append(prot_embedding)
         train_bert_embeddings["Amino Acids"].append(amino_embeddings)
 
     for seq in tqdm(sequences_test):
         encoded_input = tokenizer(seq, return_tensors='pt')
+        for key in encoded_input.keys():
+            encoded_input[key] = encoded_input[key].to(device)
         output = model(**encoded_input)
-        prot_embedding = output.pooler_output.detach().numpy()[0]
-        amino_embeddings = output.last_hidden_state.detach().numpy()[0]
-    
+        prot_embedding = output.pooler_output.cpu().numpy()[0]
+        amino_embeddings = output.last_hidden_state.cpu().numpy()[0]
+
         test_bert_embeddings["Protein"].append(prot_embedding)
         test_bert_embeddings["Amino Acids"].append(amino_embeddings)
-
 
 # Save results once for all
 
 os.makedirs('data/bert_bfd_embeddings/train/', exist_ok=True)
 os.makedirs('data/bert_bfd_embeddings/test/', exist_ok=True)
 
-with open('data/bert_bfd_embeddings/train/embeddings.pkl', 'wb') as fp:
+os.makedirs('data/bert_fine_tuned_embeddings/train/', exist_ok=True)
+os.makedirs('data/bert_fine_tuned_embeddings/test/', exist_ok=True)
+
+with open('data/bert_fine_tuned_embeddings/train/embeddings.pkl', 'wb') as fp:
     pickle.dump(train_bert_embeddings, fp)
 
-with open('data/bert_bfd_embeddings/test/embeddings.pkl', 'wb') as fp:
+with open('data/bert_fine_tuned_embeddings/test/embeddings.pkl', 'wb') as fp:
     pickle.dump(test_bert_embeddings, fp)
